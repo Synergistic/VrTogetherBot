e56e02d70ae93eb70cf1d1cff84c6d9a2342f336
Dan <Synergistic@users.noreply.github.com>
2021-04-03 20:24:18 -0400

    remove !who command logic after realizing it's effectively done via discord UI

--- bot/main.py
+++ bot/main.py
@@ -17,33 +17,15 @@ async def on_message(message):
         return
     messageContent = helpers.stripWhitespace(message.content)
     if "!help" in messageContent:
-      channel = helpers.getChannelByName(client.guilds[0].channels, "general")
+      channel = helpers.getChannelByName(client.guilds[0].channels, "rules")
       await message.author.send("Hey! Check out the " + channel.mention + " channel and read the pinned post to start playing VR with others.")
       return
     if "!commands" in messageContent:
-      await message.author.send("!help, !who, !friends, !dicks")
+      await message.author.send("!help, !friends, !dicks")
       return
     if "!friends" in messageContent:
       await message.author.send("https://steamcommunity.com/groups/vrtogether")
       return
-    if "!who" in messageContent:
-      if isinstance(message.channel, discord.channel.DMChannel):
-        await message.channel.send("!who only works in game channels")
-        return
-      memberList = ""
-      if any(message.channel.name.lower() in r.name.lower() for r in client.guilds[0].roles):
-        for member in message.guild.members:
-          if any(message.channel.name.lower() in r.name.lower() for r in member.roles) and member.name is not client.user.name:
-            memberList += member.name + ", "
-      else:
-        await message.channel.send("!who only works in game channels")
-        return
-      if len(memberList) <= 0:
-        await message.channel.send("No one is subscribed here.")
-        return
-      else:
-        await message.channel.send("The following people are subscribed to this game: " + memberList[:-2])
-        return
     if "!dicks" in messageContent:
       await message.author.send("ur a dick")
       return
