996a71a48dbfb683cb3d4f89323d87355348a047
Dan <Synergistic@users.noreply.github.com>
2021-04-13 19:49:19 -0400

    move files, add loop call to update leaderboard

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import discord
-import os 
-import helpers
-import datetime
-import constants
-import info
+import os, datetime
+import constants, helpers, info
+import discord, asyncio, requests
 
 intents = discord.Intents().all()
 intents.presences = True
@@ -12,28 +9,27 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    messageContent = helpers.stripWhitespace(message.content)
-    if "!help" in messageContent:
-      channel = helpers.getChannelByName(client.guilds[0].channels, "rules")
-      await message.author.send("Hey! Check out the " + channel.mention + " channel and read the pinned post to start playing VR with others.")
-      return
-    if "!commands" in messageContent:
-      await message.author.send("!help, !friends, !info (only works in certain game channels)")
-      return
-    if "!friends" in messageContent:
-      await message.author.send(constants.friendsText)
-      return
-    if "!info" in messageContent:
-      stuff = info.GetInfoForChannel(message.channel.name)
-      if stuff is not None:
-        await message.author.send(stuff['info'])
-        for em in stuff['embeds']:
-          await message.author.send(embed=em)
+  messageContent = helpers.stripWhitespace(message.content)
+  if "!help" in messageContent:
+    channel = helpers.getChannelByName(client.guilds[0].channels, "rules")
+    await message.author.send("Hey! Check out the " + channel.mention + " channel and read the pinned post to start playing VR with others.")
+    return
+  if "!commands" in messageContent:
+    await message.author.send("!help, !friends, !info (only works in certain game channels)")
+    return
+  if "!friends" in messageContent:
+    await message.author.send(constants.friendsText)
+    return
+  if "!info" in messageContent:
+    stuff = info.GetInfoForChannel(message.channel.name)
+    if stuff is not None:
+      await message.author.send(stuff['info'])
+      for em in stuff['embeds']:
+        await message.author.send(embed=em)
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -75,5 +71,18 @@ async def ReportedInThisChannelRecently(channel):
   botMessageWithinXMinutes = any(x for x in mostRecentMessages if x.author == client.user and ((datetime.datetime.utcnow() - x.created_at).total_seconds() / 60) <= constants.minutesBetweenMessages)
   return botMessageWithinXMinutes
 
+async def update_leaderboard():
+  run=True
+  while(run):
+    response = requests.get("https://pavlov-web-scoreboard.herokuapp.com/api/leaderboard/update", headers= {'Content-Type': 'application/json'})
+    if response.status_code == 200:
+      result = response.json()
+      if result["success"]:
+        if result["updated"]:
+          print("waiting longer cause we just updated")
+          await asyncio.sleep(580)
+    await asyncio.sleep(20)
+  
 
+client.loop.create_task(update_leaderboard())
 client.run(os.getenv('TOKEN'))
